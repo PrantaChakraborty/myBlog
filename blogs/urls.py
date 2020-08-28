9a5363ae917c1d7c645e0416f3300dc232751795
PrantaChakraborty <cbpranta@gmail.com>
2020-08-28 21:47:06 +0600

    fix blog cover problem and pagination problems

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homeView, PostDetailView
+from .views import homeView, PostDetailView, AboutView
 
 urlpatterns = [
     path('', homeView, name='home'),
-    path('post/<uuid:pk>/', PostDetailView.as_view(), name='detail_view')
+    path('post/<uuid:pk>/', PostDetailView.as_view(), name='detail_view'),
+    path('post/about_view/', AboutView.as_view(), name='about'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,17 +1,22 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Post
-from django.views.generic import DetailView
+from .models import Post, Category
+from django.views.generic import DetailView, TemplateView
 # Create your views here.
 
 
 # list view
 def homeView(request):
     post_display = Post.objects.all()
-    paginator = Paginator(post_display, 3)
+    category_display = Category.objects.all()
+    paginator = Paginator(post_display, 3) # display 3 post per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'blog/home.html', {'page_obj': page_obj})
+    context = {
+            'page_obj': page_obj,
+            'Categories': category_display
+            }
+    return render(request, 'blog/home.html', context=context)
 
 
 # detail view for blog
@@ -19,3 +24,7 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/blog_detail.html'
     context_object_name = 'post'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
