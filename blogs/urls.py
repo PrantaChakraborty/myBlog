0c88696b66f247f170aad3d24fa0dcaca1d816f1
PrantaChakraborty <cbpranta@gmail.com>
2020-08-29 22:33:31 +0600

    solve template extending problems

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import homeView, PostDetailView, AboutView, BlogCreateView, createblog
+from .views import homeView, PostDetailView, AboutView, BlogCreateView
 
 urlpatterns = [
-    path('post/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('post/about_view/', AboutView.as_view(), name='about'),
-    path('post/<uuid:pk>/', PostDetailView.as_view(), name='detail_view'),
     path('', homeView, name='home'),
+    path('post/create/', BlogCreateView.as_view(), name='create_blog'),
+    path('post/about_view/', AboutView.as_view(template_name= 'about.html'), name='about'),
+    path('post/<uuid:pk>/', PostDetailView.as_view(), name='detail_view'),
 
 ]
