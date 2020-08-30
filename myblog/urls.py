e5f74dd2e6550e9072c6bc9d19f96b8865acdc7c
PrantaChakraborty <cbpranta@gmail.com>
2020-08-30 14:56:14 +0600

    implement django authentication

--- myblog/urls.py
+++ myblog/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blogs.urls')),
-    path('accounts/', include('allauth.urls')),
+                  path('admin/', admin.site.urls),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('accounts/', include('users.urls')),
+                  path('', include('blogs.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
