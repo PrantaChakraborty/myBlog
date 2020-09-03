0bbcdf255f7cd4e2c4a811d495e4feabcc6a9bdb
PrantaChakraborty <cbpranta@gmail.com>
2020-09-03 22:07:06 +0600

    solve template problem of password change and reset view but didn't solve password change success url

--- myblog/urls.py
+++ myblog/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('accounts/', include('allauth.urls')),
                   path('accounts/', include('users.urls')),
                   path('', include('blogs.urls')),
+                  path('accounts/', include('allauth.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
 from django.urls import reverse_lazy
 from django.views import generic
-
+from allauth.account.views import PasswordChangeView
 
 from .forms import CustomUserCreationForm
 
 
 class SignUpPageView(generic.CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('account_login')
     template_name = 'account/signup.html'
+
+
+class PorfilePageView(generic.TemplateView):
+    template_name = 'users/profile.html'
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('account_login')
+
