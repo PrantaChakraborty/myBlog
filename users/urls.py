1a4fae352cadc4200cb0615c214159ae88f23976
PrantaChakraborty <cbpranta@gmail.com>
2020-09-04 10:40:39 +0600

    added success url for password change

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, re_path
-from .views import SignUpPageView, PorfilePageView, CustomPasswordChangeView
-from . import views
-
+from .views import (
+    SignUpPageView,
+    PorfilePageView,
+    CustomPasswordChangeView,
+    CustomPasswordResetFormKeyView,
+    CustomPasswordResetFormKeyDoneView
+)
 
 urlpatterns = [
-    path(r"^password/change/$", CustomPasswordChangeView.as_view(), name="account_password_change"),
+    path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+         CustomPasswordResetFormKeyView.as_view(), name="account_reset_password_from_key"),
+
+    path('password/reset/key/done/', CustomPasswordResetFormKeyDoneView.as_view(),
+         name='account_reset_password_from_key_done'),
+
+    path("password/change/", CustomPasswordChangeView.as_view(), name="account_password_change"),
+
     path('profile/', PorfilePageView.as_view(), name='profile_view'),
+
     path('signup/', SignUpPageView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from allauth.account.views import PasswordChangeView
+from allauth.account.views import PasswordChangeView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
 
 from .forms import CustomUserCreationForm
 
@@ -16,5 +16,14 @@ class PorfilePageView(generic.TemplateView):
 
 
 class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'account/password_change.html'
     success_url = reverse_lazy('account_login')
 
+
+class CustomPasswordResetFormKeyView(PasswordResetFromKeyView):
+    template_name = 'account/password_reset_form_key.html'
+    success_url = reverse_lazy('account_reset_password_from_key_done')
+
+
+class CustomPasswordResetFormKeyDoneView(PasswordResetFromKeyDoneView):
+    template_name = 'account/password_reset_form_key_done.html'
